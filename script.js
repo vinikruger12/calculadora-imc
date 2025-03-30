75f7b02f7c52dde9ab5c58e7b01c36e8ab631a1b
Vinicius Krüger <krugerdv12@gmail.com>
2025-03-30 20:51:55 -0300

    finalizado definitivo

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function descubra(){
-  res.scrollIntoView({ behavior: 'smooth' })
+  var main = document.querySelector('main')
+  main.scrollIntoView({ behavior: 'smooth' })
 }
 
 
@@ -32,39 +33,40 @@ function calcular(){
 
     if(sexoH.checked){
   if(calculo <= 18.5){
-    res.scrollIntoView({ behavior: 'smooth' })
+   h4.textContent = "Abaixo do normal"
+    mensagem.textContent = 'Procure um médico. Algumas pessoas têm um baixo peso por características do seu organismo e tudo bem. Outras podem estar enfrentando problemas, como a desnutrição. É preciso saber qual é o caso.'
     res.style.display = 'block'
     imc.innerHTML = calculo
 }else if(calculo <= 24.9){
-  res.scrollIntoView({ behavior: 'smooth' })
+  
     res.style.display = 'block'
       imc.innerHTML = calculo
         h4.textContent = "Normal"
           img.src = "imagens/normalH.webp"
             mensagem.textContent = 'Que bom que você está com o peso normal! E o melhor jeito de continuar assim é mantendo um estilo de vida ativo e uma alimentação equilibrada.'
 }else if(calculo <= 29.9){
-  res.scrollIntoView({ behavior: 'smooth' })
+ 
   res.style.display = 'block'
     imc.innerHTML = calculo
       h4.textContent = "Sobrepeso"
         img.src = "imagens/sobreH.webp"
           mensagem.textContent = 'Ele é, na verdade, uma pré-obesidade e muitas pessoas nessa faixa já apresentam doenças associadas, como diabetes e hipertensão. Importante rever hábitos e buscar ajuda antes de, por uma série de fatores, entrar na faixa da obesidade pra valer.'
 }else if(calculo <= 34.9){
-  res.scrollIntoView({ behavior: 'smooth' })
+  
   res.style.display = 'block'
     imc.innerHTML = calculo
       h4.textContent = "Obesidade grau I"
         img.src = "imagens/obeso1H.webp"
           mensagem.textContent = 'Sinal de alerta! Chegou na hora de se cuidar, mesmo que seus exames sejam normais. Vamos dar início a mudanças hoje! Cuide de sua alimentação. Você precisa iniciar um acompanhamento com nutricionista e/ou endocrinologista.'
 }else if(calculo <= 39.9){
-  res.scrollIntoView({ behavior: 'smooth' })
+ 
   res.style.display = 'block'
   imc.innerHTML = calculo
     h4.textContent = "Obesidade grau II"
       img.src = "imagens/obeso2H.webp"
         mensagem.textContent = 'Mesmo que seus exames aparentem estar normais, é hora de se cuidar, iniciando mudanças no estilo de vida com o acompanhamento próximo de profissionais de saúde.'
 }else if(calculo >= 40){
-  res.scrollIntoView({ behavior: 'smooth' })
+ 
   res.style.display = 'block'
   imc.innerHTML = calculo
     h4.textContent = "Obesidade grau III"
@@ -74,39 +76,41 @@ function calcular(){
 
 else if(sexoM.checked){
   if(calculo <= 18.5){
-    res.scrollIntoView({ behavior: 'smooth' })
+     h4.textContent = "Abaixo do normal"
+    mensagem.textContent = 'Procure um médico. Algumas pessoas têm um baixo peso por características do seu organismo e tudo bem. Outras podem estar enfrentando problemas, como a desnutrição. É preciso saber qual é o caso.'
+    img.src = "imagens/magroM.webp"
     res.style.display = 'block'
     imc.innerHTML = calculo
 }else if(calculo <= 24.9){
-  res.scrollIntoView({ behavior: 'smooth' })
+
     res.style.display = 'block'
       imc.innerHTML = calculo
         h4.textContent = "Normal"
           img.src = "imagens/normalM.webp"
             mensagem.textContent = 'Que bom que você está com o peso normal! E o melhor jeito de continuar assim é mantendo um estilo de vida ativo e uma alimentação equilibrada.'
 }else if(calculo <= 29.9){
-  res.scrollIntoView({ behavior: 'smooth' })
+
   res.style.display = 'block'
     imc.innerHTML = calculo
       h4.textContent = "Sobrepeso"
         img.src = "imagens/sobreM.webp"
           mensagem.textContent = 'Ele é, na verdade, uma pré-obesidade e muitas pessoas nessa faixa já apresentam doenças associadas, como diabetes e hipertensão. Importante rever hábitos e buscar ajuda antes de, por uma série de fatores, entrar na faixa da obesidade pra valer.'
 }else if(calculo <= 34.9){
-  res.scrollIntoView({ behavior: 'smooth' })
+  
   res.style.display = 'block'
     imc.innerHTML = calculo
       h4.textContent = "Obesidade grau I"
         img.src = "imagens/obeso1M.webp"
           mensagem.textContent = 'Sinal de alerta! Chegou na hora de se cuidar, mesmo que seus exames sejam normais. Vamos dar início a mudanças hoje! Cuide de sua alimentação. Você precisa iniciar um acompanhamento com nutricionista e/ou endocrinologista.'
 }else if(calculo <= 39.9){
-  res.scrollIntoView({ behavior: 'smooth' })
+
   res.style.display = 'block'
   imc.innerHTML = calculo
     h4.textContent = "Obesidade grau II"
       img.src = "imagens/obeso2M.webp"
         mensagem.textContent = 'Mesmo que seus exames aparentem estar normais, é hora de se cuidar, iniciando mudanças no estilo de vida com o acompanhamento próximo de profissionais de saúde.'
 }else if(calculo >= 40){
-  res.scrollIntoView({ behavior: 'smooth' })
+
   res.style.display = 'block'
   imc.innerHTML = calculo
     h4.textContent = "Obesidade grau III"
@@ -114,4 +118,5 @@ else if(sexoM.checked){
         mensagem.textContent = 'Aqui o sinal é vermelho, com forte probabilidade de já existirem doenças muito graves associadas. O tratamento deve ser ainda mais urgente.'
 }
 }}
+    res.scrollIntoView({ behavior: 'smooth' })
 }
